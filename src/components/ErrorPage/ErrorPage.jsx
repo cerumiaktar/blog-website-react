47e04e5caed25785c1da453f71b8faf2ca4a3880
unknown <ce.rumiakter@gmail.com>
2025-09-20 18:58:29 +0600

    update

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     return (
         <div className="">
             <div className="flex justify-center items-center mt-30 mb-16">
-                <img className="" src="https://i.ibb.co.com/hx6gnPSw/Frame-removebg-preview.png" alt="" />
+                <img className="" src="https://i.ibb.co.com/hx6gnPSw/Frame-removebg-preview.png" alt="404 error" />
             </div>
             <div className="text-center">
                 <Link to='/'><button className="btn bg-black text-white dark:bg-white dark:text-black">Go to Home Page</button></Link>
