cd111e1584708ad30cc7f62ef89d1b18f4654c8e
unknown <ce.rumiakter@gmail.com>
2025-11-04 15:17:14 +0600

    update

--- src/components/AllBlogs/AllBlogs.jsx
+++ src/components/AllBlogs/AllBlogs.jsx
@@ -13,7 +13,7 @@ const AllBlogs = () => {
     return (
         <div className="container mx-auto mb-12">
             <h1 className="text-2xl font-medium text-center mt-6 mb-6">All Blogs</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 {
                     allBlogs.map(allBlog => <AllBlog allBlog={allBlog}></AllBlog>)
                 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -12,7 +12,7 @@ const Blogs = () => {
     }, [])
     return (
         <div className="container mx-auto mt-12">
-            <h1 className="text-2xl font-medium text-center mb-6">Recent Blogs</h1>
+            <h1 className="text-3xl font-bold text-center mb-6">Recent Blogs</h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                 {
                     blogs.map(blog => <Blog blog={blog}></Blog>)
