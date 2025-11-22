12c4aa4fa81db78ff3f695c383c1cfab180470f0
unknown <ce.rumiakter@gmail.com>
2025-11-22 15:48:32 +0600

    update

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -17,11 +17,11 @@ const Blogs = () => {
             <h1 className="text-3xl font-bold text-center mb-6">Recent Blogs</h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                 {
-                    blogs.slice(0, 12).map(blog => <Blog blog={blog}></Blog>)
+                    blogs.slice(0, data).map(blog => <Blog blog={blog}></Blog>)
                 }
             </div>
             <div className="text-center mt-10 mb-10">
-                <button className="btn btn-outline text-lg">Load More</button>
+                <button onClick={() =>setData(blogs.length)} className="btn btn-outline text-lg">Load More</button>
             </div>
         </div>
     );
