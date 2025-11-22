038cc460335a5b3f7cafb738ad3634e856c7ff67
unknown <ce.rumiakter@gmail.com>
2025-11-22 15:43:40 +0600

    update

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,6 +5,8 @@ import Blog from "../Blog/Blog";
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
 
+    const [data, setData] = useState([12])
+
     useEffect(() => {
         fetch('blogs.json')
             .then(res => res.json())
@@ -15,7 +17,7 @@ const Blogs = () => {
             <h1 className="text-3xl font-bold text-center mb-6">Recent Blogs</h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                 {
-                    blogs.map(blog => <Blog blog={blog}></Blog>)
+                    blogs.slice(0, 12).map(blog => <Blog blog={blog}></Blog>)
                 }
             </div>
             <div className="text-center mt-10 mb-10">
