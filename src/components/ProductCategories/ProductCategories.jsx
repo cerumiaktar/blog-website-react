be0c9417a5b1f7b36b7c7cafb5afc98f2fc0a3ae
unknown <ce.rumiakter@gmail.com>
2025-11-22 17:17:47 +0600

    complete product category section

--- src/components/ProductCategories/ProductCategories.jsx
+++ src/components/ProductCategories/ProductCategories.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ProductCategory from "./ProductCategory";
 
 
 const ProductCategories = () => {
+    const [pCategories, setPCategories] = useState([])
+
+    useEffect(() =>{
+        fetch('pCategories.json')
+        .then(res=>res.json())
+        .then(data=>setPCategories(data))
+    } ,[])
     return (
         <div className="container mx-auto">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl font-bold text-center mb-6">Product Categories</h1>
                 <Link to='/products'><p>See all</p></Link>
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-6 gap-6">
+                {
+                    pCategories.map((pCategory) =><ProductCategory pCategory={pCategory}></ProductCategory>)
+                }
             </div>
         </div>
     );
--- src/components/ProductCategories/ProductCategory.jsx
+++ src/components/ProductCategories/ProductCategory.jsx
@@ -1,9 +1,20 @@
 
 
-const ProductCategory = () => {
+const ProductCategory = ({ pCategory }) => {
+    const { id, img, title } = pCategory;
     return (
         <div>
-            
+            <div className="card bg-base-100 shadow-2xl">
+                <figure className="px-10 pt-10">
+                    <img
+                        src={img}
+                        alt="Shoes"
+                        className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{title}</h2>
+                </div>
+            </div>
         </div>
     );
 };
