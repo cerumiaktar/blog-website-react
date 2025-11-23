fdc3c4765cdfea254456ce46eb26f085fc1f150c
unknown <ce.rumiakter@gmail.com>
2025-11-23 18:15:10 +0600

    add popular product

--- src/components/PopularProducts/PopularProduct.jsx
+++ src/components/PopularProducts/PopularProduct.jsx
@@ -1,9 +1,33 @@
 
 
-const PopularProduct = () => {
+
+const PopularProduct = ({ popular }) => {
+    const { id, img, title, rating, price } = popular;
+
     return (
         <div>
-            
+            <div className="card bg-base-100 text-center dark:shadow-gray-50 shadow-sm h-[550px]">
+                <figure>
+                    <img className="w-full"
+                        src={img}
+                        alt="products" />
+                </figure>
+                <div className="card-body text-center">
+                    <h2 className="card-title mx-auto">{title}</h2>
+                    <p className="text-lg text-[#F96001]">{rating}</p>
+                    {/* <div className="rating rating-xs mx-auto">
+                        <div className="mask mask-star bg-orange-400" aria-label="1 star"></div>
+                        <div className="mask mask-star bg-orange-400" aria-label="2 star"></div>
+                        <div className="mask mask-star bg-orange-400" aria-label="3 star" ></div>
+                        <div className="mask mask-star bg-orange-400" aria-label="4 star"></div>
+                        <div className="mask mask-star bg-orange-400" aria-label="5 star" aria-current="true"></div>
+                    </div> */}
+                    <p className="text-xl font-semibold">${price}</p>
+                    <div className="card-actions">
+                        <button className="btn w-full md:w-full bg-black text-white text-lg dark:border-white dark:bg-transparent">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/PopularProducts/PopularProducts.jsx
+++ src/components/PopularProducts/PopularProducts.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import PopularProduct from "./PopularProduct";
 
 
 const PopularProducts = () => {
+    const [popularProducts, setPopularProducts] = useState([])
+
+    useEffect(() =>{
+        fetch('pProducts.json')
+        .then(res=>res.json())
+        .then(data=>setPopularProducts(data))
+    } ,[])
+
     return (
         <div className="container mx-auto">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl font-bold text-center mb-6">Popular Product</h1>
                 <Link to='/products'><p>See all</p></Link>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                {
+                    popularProducts.map((popular)=><PopularProduct popular={popular}></PopularProduct>)
+                }
             </div>
         </div>
     );
