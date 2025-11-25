a079de0b7713a93a5348f02f618b0cee414e39e9
unknown <ce.rumiakter@gmail.com>
2025-11-25 12:07:01 +0600

    update all

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,37 +3,37 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero bg-base-200 dark:bg-white min-h-screen">
             <div className="hero-content flex-col">
                 <div className="text-center ">
-                    <h1 className="text-5xl font-bold">Sign Up</h1>
+                    <h1 className="text-5xl font-bold dark:text-black">Sign Up</h1>
                 </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 dark:bg-white shadow-2xl">
                     <form className="card-body">
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Name</span>
+                                <span className="label-text dark:text-black">Name</span>
                             </label>
-                            <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                            <input type="text" name='name' placeholder="Name" className="input input-bordered dark:bg-white dark:text-[#333333] dark:border dark:border-[#333333]" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text dark:text-black">Email</span>
                             </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered dark:bg-white dark:text-[#333333] dark:border dark:border-[#333333]" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text dark:text-black">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered dark:bg-white dark:text-[#333333] dark:border dark:border-[#333333]" required />
                             
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn w-full bg-black text-white text-lg dark:bg-white dark:text-black dark:border-none">Sign Up</button>
+                            <button className="btn w-full bg-black text-white text-lg dark:bg-black dark:border-none">Sign Up</button>
                         </div>
                         <div>
-                            <p>Already have an account <Link className="text-blue-700 font-bold" to='/signin'>Signin</Link></p>
+                            <p className='dark:text-black'>Already have an account <Link className="text-blue-700 font-bold" to='/signin'> Signin</Link></p>
                         </div>
                     </form>
                 </div>
